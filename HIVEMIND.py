b21d420a2105ff42f8ef90574bbbb1949df8fa73
Stemmis <theblazzer21@gmail.com>
2021-08-16 13:46:28 -0400

    Update HIVEMIND.py

--- HIVEMIND.py
+++ HIVEMIND.py
@@ -5,25 +5,25 @@ import sys
 from discord import Client, Intents, Embed
 from discord_slash import SlashCommand, SlashContext
 
-client = discord.Client() #The bot itself
+client = commands.Bot() #The bot itself
 bot = Client(intents=Intents.default())
-slash = SlashCommand(bot) #Implements slash commands
+slash = SlashCommand(client) #Implements slash commands
 
-@slash.event
+@client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(slash))
+    print('We have logged in as {0.user}'.format(client))
 
 @slash.slash(name="test")
 async def test(ctx: SlashContext):
     embed = Embed(title="Embed Test")
     await ctx.send(embed = embed)
 
-@slash.event
+@client.event
 async def on_message(message):
-    if message.author == slash.user:
+    if message.author == client.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-slash.run(str(sys.argv[1])) #Use token as argument when running script from console.
+client.run(str(sys.argv[1])) #Use token as argument when running script from console.
