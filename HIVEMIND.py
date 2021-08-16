b67d458d8ad5dd7222bf738e8983043f2c8d8e35
Stemmis <theblazzer21@gmail.com>
2021-08-16 13:33:24 -0400

    Slash commands

--- HIVEMIND.py
+++ HIVEMIND.py
@@ -1,11 +1,18 @@
 import discord
+import discord_slash
 import sys
 
-client = discord.Client()
+client = discord.Client() #The bot itself
+slash = SlashCommand(client) #Implements slash commands
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
+@slash.slash(name="test")
+async def test(ctx: SlashContext):
+    embed = Embed(title="Embed Test")
+    await ctx.send(embed = embed)
 
 @client.event
 async def on_message(message):
