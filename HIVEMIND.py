bd744f6c6c6ed87c2f7bd34f0c261c68691ae4a8
Stemmis <theblazzer21@gmail.com>
2025-06-27 22:45:34 -0400

    test removing async def init()

--- HIVEMIND.py
+++ HIVEMIND.py
@@ -838,7 +838,7 @@ async def rollwod(ctx, pool:int, difficulty:int=6, modifier:int=0, crits:bool=Fa
 #Adds a new entry to the initiative database
 #Says the encounter ID in chat
 @interactions.slash_command(name = "init")
-async def init(ctx: interactions.SlashContext):
+#async def init(ctx: interactions.SlashContext):
     #"Initiative family of commands"
     #pass
     
